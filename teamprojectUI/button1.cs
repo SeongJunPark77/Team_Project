4499b7e77e88d34b974c362849a32836c540bfb6
sj3048076@naver.com <sj3048076@naver.com>
2020-05-03 23:06:57 +0900

    TEAMPROJECT_HW
    SJ's UI HW

---
+++ teamprojectUI/button1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player1;
+    public GameObject player2;
+    public GameObject player3;
+
+    void Start()
+    {
+        player1.SetActive(true);
+        player2.SetActive(false);
+        player3.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnButtonClick1()
+    {
+        player1.SetActive(true);
+        player2.SetActive(false);
+        player3.SetActive(false);
+    }
+}
