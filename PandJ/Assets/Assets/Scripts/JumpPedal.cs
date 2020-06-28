ea62b0d99019916455ac5c721a3c574b67f20239
seokjae123 <tjrwo9090@gmail.com>
2020-06-28 15:56:32 +0900

    파일경로 정리
    폴더 정리함

---
+++ PandJ/Assets/Assets/Scripts/JumpPedal.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPedal : MonoBehaviour
+{
+    void Start()
+    {
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            
+        }
+    }
+}
