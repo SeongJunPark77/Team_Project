968328d1af10b5627a10eac8d43694c45f9998b8
sj3048076@naver.com <sj3048076@naver.com>
2020-06-28 19:58:01 +0900

    TeamProject
    script

---
+++ PandJ/Assets/object_moving.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class object_moving : MonoBehaviour
+{
+    public float distance_max;
+    public float distance_min;
+
+    public float direction;
+
+    float position_x; 
+    float position_y; 
+    float position_z;
+
+    void Start()
+    {
+        position_x = this.transform.position.x;
+        position_y = this.transform.position.y;
+        position_z = this.transform.position.z;
+    }
+
+    void Update()
+    {
+        position_z += Time.deltaTime * direction;
+
+        if (position_z >= distance_max)
+        {
+            direction *= -1;
+            position_z = distance_max;
+        }
+        else if (position_z <= distance_min)
+        {
+            direction *= -1;
+            position_z = distance_min;
+        }
+
+        this.transform.position = new Vector3(position_x, position_y, position_z);
+    }
+}
