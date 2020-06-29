02573e65ebd89b65d7a7a43155089d3c0b3795be
seokjae123 <tjrwo9090@gmail.com>
2020-06-30 04:49:25 +0900

    움직이는 발판 코드 수정

---
+++ PandJ/Assets/Assets/Scripts/object_moving_y.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class object_moving_y : MonoBehaviour
+{
+    Vector3 pos;
+
+    public float delta = 0.0f;
+    public float speed = 0.0f;
+
+    void Start()
+    {
+        pos = transform.position;
+    }
+
+    void Update()
+    {
+        Vector3 v = pos;
+        v.y += delta * Mathf.Sin(Time.time * speed);
+        transform.position = v;
+    }
+}
---
+++ PandJ/Assets/Assets/Scripts/object_moving_z.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class object_moving_z : MonoBehaviour
+{
+    Vector3 pos;
+
+    public float delta = 0.0f;
+    public float speed = 0.0f;
+
+    void Start()
+    {
+        pos = transform.position;
+    }
+
+    void Update()
+    {
+        Vector3 v = pos;
+        v.z += delta * Mathf.Sin(Time.time * speed);
+        transform.position = v;
+    }
+}
